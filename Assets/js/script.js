2f14c00399cc72bb0a9b358e58c3c0289bee145f
Katie Maddox <kmaddox2122@gmail.com>
2022-10-18 16:30:26 -0500

    completed current day weather data pulled by api, pending stying.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,17 +11,70 @@
 //write function to link search history location to current and future weather for that city
 
 var apiKey = config.apiKey;
-var city = "Houston";
-var requestLatLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey ;
-//var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey;
+var city ='';
 
+var fetchButton = document.querySelector(".btn");
 
-fetch(requestLatLon)
+function getApi (){
+  city = document.getElementById('search-input').value;
+  console.log(city);
+  var requestLatLon = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey ;
+
+  fetch(requestLatLon)
   .then(function (response) {
-    console.log(response.json());
-    return;
- })
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("lat and lon");
+    getWeather(data[0].lat, data[0].lon);
+  });
+}
 
+function getWeather (lat,lon) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey;
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var date = data.list[0].dt_txt.split(' ')[0].split('-');
+      date = date[1] + '/' + date[2] + '/' + date[0];
+      console.log(date);
+      var datecontainer = document.getElementById('dateResults');
+      datecontainer.innerHTML = `
+      ${date}`
+    var temp = data.list[0].main.temp
+      // ${data.list[0].main.temp}*F
+      var tempContainer = document.getElementById('tempResults');
+      tempContainer.innerHTML = `
+      ${temp}`
+    var windSpeed = data.list[0].wind.speed;
+      console.log(windSpeed);
+      var windContainer = document.getElementById('windResults');
+      windContainer.innerHTML = `
+      ${windSpeed}`
+    var humidity = data.list[0].main.humidity;
+      console.log(humidity);
+      var humidityContainer = document.getElementById('humidityResults');
+      humidityContainer.innerHTML = `
+      ${humidity}`
+    var cityName = data.city.name;
+      console.log(cityName);
+      var cityNameContainer = document.getElementById('locationResults');
+      cityNameContainer.innerHTML = `
+      ${cityName}`
+  });
+}
 
+fetchButton.addEventListener('click', getApi);
 
+//for 5 day forecast- will need to pull every 8th object in the array
 
+  /* use async and await fetch to store data from api fetch in a variable. 
+  async function getApi(requestUrl) {
+    let response = await fetch(requestUrl);
+    let prettyData = await response.json();
+    return prettyData;
+  }   
+*/
